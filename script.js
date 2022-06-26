eff3682dd6ca16f8586e7c197cf6576060e90406
filipe <filipe.ogramlich@gmail.com>
2022-06-25 23:50:22 -0300

    Revert "first-refactoring"
    This reverts commit 1b00cbe897bfdf6ff580e5f3e3d88276f24643fd.

--- script.js
+++ script.js
@@ -4,53 +4,12 @@ const buttonStop = document.querySelector(".stop")
 const buttonSet = document.querySelector(".set")
 const buttonSound_on = document.querySelector(".sound-on")
 const buttonSound_off = document.querySelector(".sound-off")
-let minutes
-const minutesDisplay = document.querySelector(".minutes")
-const secondsDisplay = document.querySelector(".seconds")
-
-function resetControls(){
-    buttonStop.classList.add("hide")
-    buttonSet.classList.remove("hide")
-    buttonPause.classList.add("hide")
-    buttonPlay.classList.remove("hide")
-}
-
-function updateTimerDisplay (minutes,seconds){
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-    secondsDisplay.textContent = String(seconds).padStart(2,"0")
-}
-
-function coutdown(){
-    setTimeout(function(){
-
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-
-        updateTimerDisplay(minutes, 0)
-        
-        if(minutes <= 0){
-            resetControls()
-            return  
-        }
-
-        if (seconds <= 0 ){
-            seconds = 60
-            --minutes
-        }
-
-        updateTimerDisplay(minutes,String(seconds-1))
-        
-        coutdown()
-    }, 1000)
-}
 
 buttonPlay.addEventListener("click",function (){
     buttonPlay.classList.add("hide") 
     buttonPause.classList.remove("hide")    
     buttonSet.classList.add("hide")
     buttonStop.classList.remove("hide")
-    
-    coutdown()
 })
 buttonPause.addEventListener("click",function (){
     buttonPause.classList.add("hide")    
@@ -58,7 +17,10 @@ buttonPause.addEventListener("click",function (){
     
 })
 buttonStop.addEventListener("click",function (){
-    resetControls()
+    buttonStop.classList.add("hide")
+    buttonSet.classList.remove("hide")
+    buttonPause.classList.add("hide")
+    buttonPlay.classList.remove("hide")
 })
 buttonSound_on.addEventListener("click",function (){
     buttonSound_on.classList.add("hide")
@@ -67,8 +29,4 @@ buttonSound_on.addEventListener("click",function (){
 buttonSound_off.addEventListener("click",function (){
     buttonSound_off.classList.add("hide")
     buttonSound_on.classList.remove("hide")
-})
-buttonSet.addEventListener("click",function (){
-    minutes = prompt("Quantos minutos são?")
-    updateTimerDisplay(minutes,0)
 })
